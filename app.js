a8866e73c38f27fc53b458e89598614adfa5b343
Juan Doussett <jdoussett@gmail.com>
2025-09-21 18:03:34 -0300

    Se agregó la función agregarAmigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    amigoAgregado = document.getElementById('amigo').value;
+
+    if (amigoAgregado == "") {
+        alert("Por favor, agregue a un amigo.");
+    } else {
+        amigos.push(amigoAgregado);
+        limpiarCaja();
+        //console.log(amigos);
+    }
+}
